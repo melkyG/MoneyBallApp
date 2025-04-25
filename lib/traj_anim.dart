2d33c2ef8716ab275db22e0fed208637150dde68
mahalm <m_mammadli@hotmail.com>
2025-04-24 21:56:18 -0500

    mod ball trajectory

--- lib/traj_anim.dart
+++ lib/traj_anim.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 class BallTrajectory extends StatefulWidget {
   final bool shotMade;
@@ -19,13 +20,14 @@ class _BallTrajectoryState extends State<BallTrajectory>
     with SingleTickerProviderStateMixin {
   late AnimationController _controller;
   late Animation<Offset> _trajectory;
+  late Animation<double> _scale; // Added for scaling animation
 
   @override
   void initState() {
     super.initState();
 
     _controller = AnimationController(
-      duration: const Duration(milliseconds: 600),
+      duration: const Duration(milliseconds: 1000),
       vsync: this,
     )..addStatusListener((status) {
         if (status == AnimationStatus.completed) {
@@ -33,13 +35,28 @@ class _BallTrajectoryState extends State<BallTrajectory>
         }
       });
 
-    // Example curve: you can replace this with Bezier or physics-based
+    // Trajectory animation
     _trajectory = Tween<Offset>(
       begin: Offset(0.0, 1.0), // Start near bottom
-      end: widget.shotMade ? Offset(0.0, 0.0) : Offset(-0.3, 0.1),
+      end: widget.shotMade
+          ? Offset(0.0, -1.95)
+          : [
+              Offset(-0.2, -2.5), // Base value
+              Offset(0.2, -3.0), // Base value
+              Offset(0.5, -2.4), // Base value
+            ][Random().nextInt(3)],
     ).animate(CurvedAnimation(
       parent: _controller,
       curve: Curves.easeOut,
+    ));
+
+    // Scale animation: from 1.0 to 0.8
+    _scale = Tween<double>(
+      begin: 1.1, // Full size at start
+      end: 0.75, // 75% size at end
+    ).animate(CurvedAnimation(
+      parent: _controller,
+      curve: Curves.easeOut, // Same curve as trajectory for smoothness
     ));
 
     _controller.forward();
@@ -51,10 +68,13 @@ class _BallTrajectoryState extends State<BallTrajectory>
       animation: _controller,
       builder: (context, child) {
         return Transform.translate(
-          offset: _trajectory.value * 300,
-          child: Image.asset(
-            "assets/images/ball.png",
-            width: 24,
+          offset: _trajectory.value * 75,
+          child: Transform.scale(
+            scale: _scale.value, // Apply scale animation
+            child: Image.asset(
+              "assets/images/ball.png",
+              width: 24,
+            ),
           ),
         );
       },
